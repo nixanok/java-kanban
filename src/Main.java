d307925bb761083c99010201fccf94794eb52944
nixanok <mrfreemen555@gmail.com>
2023-03-30 19:30:01 +0300

    refactor: <переработать логику сабтасков> delete: <удалить метод добавления сабтаска в эпик> perf: <оптимизировать методы с использованием обновления статуса эпика>

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,6 @@
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
-        HashMap<Integer, String> map = new HashMap<>();
-        map.put(1, "привет");
-        map.put(2, "пока");
-
-        ArrayList<String> s =  new ArrayList<>(map.values());
-        System.out.println(s);
-
     }
 }
